efc15c95f326df5d8c8791980a4e50c944f1025f
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2020-12-07 07:06:27 -0500

    add heroku http to channel layer settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -135,7 +135,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('organize-semaphore.herokuapp.com', '127.0.0.1', 6379)],
         },
     },
 }
