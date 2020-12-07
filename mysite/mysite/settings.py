7a527e3093e0f7c2bcdd41ff23abeac1fa972dbd
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2020-12-07 07:32:21 -0500

    add import django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django
 from pathlib import Path
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
