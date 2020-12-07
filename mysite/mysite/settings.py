aa10a786188fba1f7ee776704de75d51014ff600
Erika Fasankova <xfasan00@stud.fit.vutbr.cz>
2020-12-07 06:16:05 -0500

    fixed local collectstatic

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
