ea03e4dbafce79b5d8a2275f1ccdeed4dea82067
7Angie7 <43541116+7Angie7@users.noreply.github.com>
2020-12-07 09:46:47 -0500

    Update asgi.py

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -7,7 +7,7 @@ from django.core.asgi import get_asgi_application
 import semaphore.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-export DJANGO_SETTINGS_MODULE=mysites.settings
+
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
