bbf41d77fb72f6caab2d159e2f3d56b602555073
7Angie7 <43541116+7Angie7@users.noreply.github.com>
2020-12-07 06:10:07 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,10 +124,9 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../semaphore/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DISABLE_COLLECTSTATIC=1
 
 # Channels
 ASGI_APPLICATION = 'mysite.asgi.application'
